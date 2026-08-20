2e61ab5a5be0746528746f3836c523ffc821c50d
Blessed-BCN <esparkic@gmail.com>
2026-08-20 17:52:14 +0200

    Deploy inicial App Tatuador a GitHub Pages

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'blessed-tatuador-v2.15.4';
+const ASSETS = ['/tatuador/', '/tatuador/index.html', '/tatuador/manifest.json', '/tatuador/icon.svg', '/tatuador/icons/icon-192.png', '/tatuador/icons/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+      .then(() => self.clients.matchAll({ type: 'window', includeUncontrolled: true }))
+      .then(clients => clients.forEach(c => c.postMessage({ type: 'SW_UPDATED' })))
+  );
+});
+
+// Network-first: sempre intenta xarxa, si falla serveix cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
